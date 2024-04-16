da283837a48996ce7c18bb74a4c0093013855605
Rocky Kuang <shrkuang@gmail.com>
2024-04-16 11:31:12 +0200

    scope:
    - adjusted some elements after rerunning tests
    
    - conclusion  = flaky test , although the setup is fine. Locators of AH.nl website has changed. Also, ah website in prod is not a great website to run automated tests on it since they can detect such type of tests.

--- resources/pageObjects/Navbar.py
+++ resources/pageObjects/Navbar.py
@@ -1,7 +1,7 @@
 class Navbar:
     def __init__(self, page):
         self.page = page
-        self.products = page.locator('//a[@data-testhook="navigation-products"]')
+        self.products = page.locator('//button[@data-testhook="navigation-products"]/span')
         self.account = page.locator('//div[@class="user-icon_container__5+nqK icon-button_icon__qsWNH"]')
 
     def click_on_products(self):
